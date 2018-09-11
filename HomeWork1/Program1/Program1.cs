493bd9dd0d265cd1bfbd18fcd3e1daecbb2aa113
g2502823040 <2502823040@qq.com>
2018-09-11 12:10:16 +0800

    first commit

---
+++ HomeWork1/Program1/Program1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input a:");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Please input b:");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("a*b="+a*b);
+            Console.ReadKey();
+        }
+    }
+}
