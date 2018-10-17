4fb8530e97c789f3e79133937e78196bef5decdd
g2502823040 <2502823040@qq.com>
2018-10-17 13:06:39 +0800

    fifth commit

---
+++ Homework5/program1/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    public class Order
+    {
+        public void Start(OrderService a)
+        {
+            bool Y = true;
+            while (Y)
+            {
+                Console.WriteLine("请输入：1.添加订单 2.删除订单 3.更改订单 4.查找订单 5.完成订单");
+                string n = Console.ReadLine();
+                switch (n)
+                {
+                    case "1": a.Add(); break;
+                    case "2": a.Delete(); break;
+                    case "3": a.Change(); break;
+                    case "4": a.Find(); break;
+                    case "5": Y = false; break;
+                    default:
+                        Console.WriteLine("无效操作");
+                        break;
+                }
+            }
+        }
+    }
+    public class OrderDetails
+    {
+        public int id;
+        public string name;
+        public string customer;
+        public int money;
+    }
+    public class OrderService
+    {
+        public List<OrderDetails> orderlist = new List<OrderDetails>();
+        public void Add()
+        {
+            OrderDetails a = new OrderDetails();
+            Console.WriteLine("请输入订单号码：");
+            a.id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("请输入订单名字：");
+            a.name = Console.ReadLine();
+            Console.WriteLine("请输入客户名字：");
+            a.customer = Console.ReadLine();
+            Console.WriteLine("请输入订单金额：");
+            a.money = Convert.ToInt32(Console.ReadLine());
+            orderlist.Add(a);
+        }
+        public void Delete()
+        {
+            try
+            {
+                Console.WriteLine("请输入删除订单的号码：");
+                int b = Convert.ToInt32(Console.ReadLine());
+                orderlist.RemoveAt(b - 1);
+            }
+            catch { Console.WriteLine("无效操作"); }
+        }
+        public void Change()
+        {
+            OrderDetails a = new OrderDetails();
+            Console.WriteLine("请输入更改订单的号码：");
+            a.id = Convert.ToInt32(Console.ReadLine());
+            foreach (OrderDetails c in orderlist)
+            {
+                if (a.id == c.id)
+                {
+                    Console.WriteLine("请输入新订单名字：");
+                    a.name = Console.ReadLine();
+                    Console.WriteLine("请输入新顾客名字：");
+                    a.customer = Console.ReadLine();
+                }
+            }
+        }
+        public void Find()
+        {
+            OrderDetails a = new OrderDetails();
+            Console.WriteLine("请输入：1.按号码查找 2.按名字查找 3.按顾客查找 4.按金额查找");
+            int c = Convert.ToInt32(Console.ReadLine());
+            switch (c)
+            {
+                case 1:
+                    Console.WriteLine("请输入查找订单的号码：");
+                    a.id = Convert.ToInt32(Console.ReadLine());
+                    var result1 = from i in orderlist where i.id == a.id select i;
+                    foreach(var i in result1)
+                    {
+                        Console.WriteLine("订单号码:" + i.id + "订单名称：" + i.name +
+                            "订单顾客：" + i.customer + "订单金额" + i.money);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("请输入查找订单的名字：");
+                    a.name = Console.ReadLine();
+                    var result2 = from i in orderlist where i.name == a.name select i;
+                    foreach (var i in result2)
+                    {
+                        Console.WriteLine("订单号码：" + i.id + "订单名称：" + i.name +
+                            "订单顾客：" + i.customer + "订单金额" + i.money);
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("请输入查找订单的顾客：");
+                    a.customer = Console.ReadLine();
+                    var result3 = from i in orderlist where i.customer == a.customer select i;
+                    foreach (var i in result3)
+                    {
+                        Console.WriteLine("订单号码：" + i.id + "订单名称：" + i.name +
+                            "订单顾客：" + i.customer + "订单金额" + i.money);
+                    }
+                    break;
+                case 4:
+                    Console.WriteLine("请输入查找订单金额大于：");
+                    a.id = Convert.ToInt32(Console.ReadLine());
+                    var result4 = from i in orderlist where i.money > a.money select i;
+                    foreach (var i in result4)
+                    {
+                        Console.WriteLine("订单号码：" + i.id + "订单名称：" + i.name +
+                            "订单顾客：" + i.customer + "订单金额" + i.money);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("无效操作");
+                    break;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            OrderService o = new OrderService();
+            order.Start(o);
+        }
+    }
+}
---
+++ Homework5/program2/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Graphics graphics;
+        int n;
+        int LineWidth;
+        double leng;
+        double x0;
+        double y0;
+        double th1;
+        double th2;
+        double per1=0.6;
+        double per2=0.7;
+        void drawCayleyTree(int n, double x0, double y0, double leng,  double th)
+        {
+            double temp1= double.Parse(textBox1.Text);
+            double temp2 = double.Parse(textBox3.Text);
+            double th1 = temp1 * Math.PI / 180;
+            double th2 = temp2 * Math.PI / 180;
+            if (0 == n) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            drawLine(x0, y0, x1, y1);
+            drawCayleyTree(n - 1, x1, y1, per1*leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2*leng, th - th2);
+        }
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            int LineWidth = int.Parse(textBox4.Text);
+            if (checkBox1.Checked)
+            {
+                Pen pen = new Pen(Color.Red, LineWidth);
+                graphics.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1);
+            }
+            else if (checkBox2.Checked)
+            {
+                Pen pen = new Pen(Color.Yellow, LineWidth);
+                graphics.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1);
+            }
+            else if (checkBox3.Checked)
+            {
+                Pen pen = new Pen(Color.Blue, LineWidth);
+                graphics.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1);
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox5.Text);
+            double x0 = double.Parse(textBox6.Text);
+            double y0 = double.Parse(textBox7.Text);
+            int leng = int.Parse(textBox2.Text);
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(n, x0, y0, leng, -Math.PI / 2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.White);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
